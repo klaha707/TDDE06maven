5beab7b8c2a6a4cf1246eaaeda231fecc14fe0e5
Klas Harrysson <klaha707@student.liu.se>
2018-04-13 13:50:52 +0200

    add test framework for go

---
+++ client/src/main/java/TimeRetriever.java
@@ -0,0 +1,13 @@
+package main;
+import org.joda.time.LocalTime;
+import org.apache.log4j.Logger;
+
+public class TimeRetriever {
+    public static String getTimeMessage() {
+        Logger logger = Logger.getLogger(HelloMaven.class);
+        LocalTime localTime = new LocalTime();
+
+        logger.info("Requested time string: " + localTime.toString());
+        return "TODO list: " + localTime.toString();
+    }
+}
