819035b283a5d3ebb150518173b40008e0fb8880
Cecilia <ceciliamalmrud@gmail.com>
2018-04-25 15:05:35 +0200

    added java code in plugin

--- bestpluginever-maven-plugin/src/main/java/GreetingMojo.java
+++ bestpluginever-maven-plugin/src/main/java/GreetingMojo.java
@@ -3,16 +3,40 @@ package main.java;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import java.io.*;
  
 /**
  * Says "Hi" to the user.
  *
  */
+
+
 @Mojo( name = "sayhi")
 public class GreetingMojo extends AbstractMojo
 {
     public void execute() throws MojoExecutionException
     {
         getLog().info( "Hello, world." );
+        /**
+         * My File.
+         */
+        
+        String text = "Hello TDDE06";
+        BufferedWriter output = null;
+        try {
+        	File myFile = new File("file.txt");
+        	output = new BufferedWriter(new FileWriter(myFile));
+        	output.write(text);
+        } catch (IOException e) {
+        	e.printStackTrace();
+        } finally {
+        	if(output != null) {
+        		try {
+        			output.close();
+        		} catch (IOException e) {
+                	e.printStackTrace();
+                }
+        	}
+        }
     }
 }
